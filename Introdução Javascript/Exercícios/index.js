f8196ea89402be4100ab46be95d56ffb18f9cbcf
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2021-07-15 19:46:25 -0300

    Exercício de console.log terminado

---
+++ Introdução Javascript/Exercícios/index.js
@@ -0,0 +1,8 @@
+/* Exercício: Escrever a frase: "Meu nome é "NOME". Estou aprendendo Javascript às 10 da manhã".
+    o número 10 deve ser lido como um Number normal, então deve estar fora de áspas duplas.
+    O nome deve ser lido dentro das áspas porém como se fosse uma variável.
+*/
+
+var nome = 'Gustavo';
+
+console.log("Meu nome é '", nome, "'. Estou aaprendendo Javascript às ", 10, "h da manhã!")

ace398e3a99651ab8711856908ef2c4c9e21b12d
Guga-Dev10 <gustavo.souza.dev10@gmail.com>
2021-07-20 20:51:24 -0300

    Terminada a integração de html com js

---
+++ Introdução Javascript/Navegador vs Node (HTML + Js)/index.js
@@ -0,0 +1,21 @@
+console.log("Olá mundo!");
+console.log("Trecho para ser exibido no navegador");
+console.log('Usando um arquivo index.js');
+
+/**
+ *  No amibiente do navegor nós manipulamos o DOM e 
+ * alguns elementos da página, o motor usado está vinculado
+ * ao navegador.
+ *  No ambiente de desenvovimento utilizamos o Node para
+ * conseguirmos desenvolver e executar o código, porém
+ * alguns comandos que funcionam no ambiente do navegador
+ * não funcionam da mesma forma que no ambiente de desenvolvimento,
+ * como é o caso do comando alert
+ */
+
+alert('Olá, meu nome é Jarvis!');
+/**
+ *  Este comando no editor não executa um código e ainda
+ * retorna um erro. No navegador, este comando irá funcionar
+ * e abrirá uma caixa de diálogo na página.
+ */
